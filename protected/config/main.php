bdba4cf43f7ff2c0cc5a2f0c1a1f3420994b1bc2
Neeraj Kumar <neeraj24a@gmail.com>
2015-11-17 02:36:33 +0530

    Conflict Resolve
    Conflicts:
    	protected/config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,10 +5,7 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 require_once( dirname(__FILE__) . '/../components/helpers.php');
-<<<<<<< HEAD
 require(dirname(__FILE__).'/global.php');
-=======
->>>>>>> origin/master
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'My Web Application',
